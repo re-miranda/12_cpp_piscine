58c2eab1cd4892fd9234a5f7a38c04f0a360493a
remiranda <pesca_botoes.0t@icloud.com>
2024-04-26 01:00:13 -0300

    preliminary input filters

--- module_09/ex02/main.cpp
+++ module_09/ex02/main.cpp
@@ -1,8 +1,32 @@
+#include <algorithm>
 #include <iostream>
 #include "PmergeMe.hpp"
 
-int	main(void)
+bool	_validate_input( std::string const & input );
+
+int	main(int argc, char *argv[])
 {
-	PmergeMe	A;
+	int	ix;
+	PmergeMe		A;
+
+	if (argc < 2) {
+		std::cout << "Error: bad arguments" << std::endl;
+		return (-1);
+	}
+	ix = 0;
+	while (++ix < argc)
+		if (_validate_input(argv[ix]))
+			return (-2);
 	return (0);
+}
+
+bool	_validate_input( std::string const & input ) {
+	std::size_t	result;
+
+	result = input.find_first_not_of("1234567890");
+	if (result != std::string::npos) {
+		std::cout << "Error: Invalid char in input => " << input[result] << std::endl;
+		return (true);
+	}
+	return (false);
 }
